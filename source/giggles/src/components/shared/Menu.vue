c4aae2bdfb5b5b4908bf0da43bbc3939eadece99
Jessica Nierth <jnierth@web.de>
2018-05-06 18:53:51 +1000

    (feat) Included Bootstrap 4 main menu.

--- source/giggles/src/components/shared/Menu.vue
+++ source/giggles/src/components/shared/Menu.vue
@@ -1,33 +1,56 @@
 <template>
   <div>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <router-link :to="{name:'Home'}" class="navbar-brand">
-        <img class="logo" src="../../assets/logo.png" alt="logo"/>
-      </router-link>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <router-link class="nav-link" active-class="active" :to="{name:'Home'}">Home <span class="sr-only"></span></router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" active-class="active" :to="{name:'About'}">About <span class="sr-only"></span></router-link>
-          </li>
-        </ul>
-      </div>
-    </nav>
+
+    <b-navbar toggleable="md" type="dark" variant="info">
+
+      <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+
+      <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+      <b-collapse is-nav id="nav_collapse">
+
+        <b-navbar-nav>
+          <!--<router-link :to="{name:'Home'}" class="navbar-brand">-->
+            <!--<img class="logo" src="../../assets/logo.png" alt="logo"/>-->
+          <!--</router-link>-->
+          <router-link class="nav-link" exact active-class="active" :to="{name:'Home'}">Home <span class="sr-only"></span></router-link>
+          <router-link class="nav-link" active-class="active" :to="{name:'About'}">About <span class="sr-only"></span></router-link>
+        </b-navbar-nav>
+
+      </b-collapse>
+    </b-navbar>
+
   </div>
 </template>
 
-<style scoped lang="scss">
+<script>
+  // import Navbar from '../../../node_modules/bootstrap-vue/src/components/navbar'
+  // import Navbar from '../../../node_modules/bootstrap-vue/src/components/navbar'
+  // import bNavbar from '../../../node_modules/bootstrap-vue/es/components/navbar/navbar'
+  // import bNavbarBrand from '../../../node_modules/bootstrap-vue/es/components/navbar/navbar-brand'
+  // import bCollapse from '../../../node_modules/bootstrap-vue/es/components/collapse'
+  // import bDropdown from '../../../node_modules/bootstrap-vue/es/components/dropdown'
+  //
+  // export default {
+  //   name: 'Menu',
+  //   components: {
+  //     bNavbarBrand,
+  //     bCollapse}, //Register other components,
+  //   directives: {
+  //     'b-navbar': bNavbar,
+  //     'b-navbar-brand': bNavbarBrand,
+  //     'b-collapse': bCollapse,
+  //     'b-dropdown': bDropdown
+  //   }
+  // }
+</script>
+<style lang="scss">
   @import '../../assets/styles/vendors/bootstrap/functions';
   @import '../../assets/styles/vendors/bootstrap/variables';
-  @import "../../assets/styles/vendors/bootstrap/mixins/breakpoints";
-  @import "../../assets/styles/vendors/bootstrap/mixins/hover";
-  @import "../../assets/styles/vendors/bootstrap/mixins/border-radius";
+  @import '../../assets/styles/vendors/bootstrap/mixins';
   @import "../../assets/styles/vendors/bootstrap/navbar";
+</style>
+<style scoped lang="scss">
   .logo {
     max-height: 40px;
   }
--- source/giggles/src/main.js
+++ source/giggles/src/main.js
@@ -4,15 +4,24 @@ import Vue from 'vue'
 import App from './App'
 import router from './plugins/router'
 import VueResource from './plugins/vue-resource'
+
+
+import Nav from '../node_modules/bootstrap-vue/es/components/nav';
+import Navbar from '../node_modules/bootstrap-vue/es/components/navbar';
+
 import Menu from '@/components/shared/Menu'
 import Footer from '@/components/shared/Footer'
 import Fun from '@/components/Fun'
+
 
 Vue.config.productionTip = false
 
 Vue.component('app-footer', Footer)
 Vue.component('app-menu', Menu)
 Vue.component('app-fun', Fun)
+
+Vue.use(Nav)
+Vue.use(Navbar)
 
 /* eslint-disable no-new */
 new Vue({
